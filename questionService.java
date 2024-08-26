2d7c09b9bab1eb92c3154dfd8dd9524fc3942ced
AshwiniKumar1103 <ashwini.kumar1fdbeb@cognizant.com>
2024-08-26 12:00:26 +0530

    Result checked And marks Displayed

--- questionService.java
+++ questionService.java
@@ -1,35 +1,74 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
+
 public class questionService {
-    int [] UserInput=new int[5];
-    Question [] questionz = new Question[5];
-    questionService(){
-        questionz[0]=new  Question(1,"what is capital of India",new String[]{"delhi","kolkata","goa","bangalore"},1);
-        questionz[1]=new  Question(2,"what is capital of India",new String[]{"delhi","kolkata","goa","bangalore"},1);
-        questionz[2]=new  Question(3,"what is capital of India",new String[]{"delhi","kolkata","goa","bangalore"},1);
-        questionz[3]=new  Question(4,"what is capital of India",new String[]{"delhi","kolkata","goa","bangalore"},1);
-        questionz[4]=new  Question(5,"what is capital of India",new String[]{"delhi","kolkata","goa","bangalore"},1);
+    int[] UserInput = new int[5];
+    private int z = 0;
+    private int Score=0;
+    private int earlyResultCounter=0;
+    Question[] questionz = new Question[5];
+
+    questionService() {
+        questionz[0] = new Question(1, "what is capital of India",
+                new String[] { "delhi", "kolkata", "goa", "bangalore" }, 1);
+        questionz[1] = new Question(2, "what is capital of India",
+                new String[] { "delhi", "kolkata", "goa", "bangalore" }, 1);
+        questionz[2] = new Question(3, "what is capital of India",
+                new String[] { "delhi", "kolkata", "goa", "bangalore" }, 1);
+        questionz[3] = new Question(4, "what is capital of India",
+                new String[] { "delhi", "kolkata", "goa", "bangalore" }, 1);
+        questionz[4] = new Question(5, "what is capital of India",
+                new String[] { "delhi", "kolkata", "goa", "bangalore" }, 1);
     }
-    public void Continue(int a,int _z){
-        if(a>4){
+
+    public int Continue(int a, int _z, int k) {
+        if (a > 4) {
+            k++;
+            if (k > 2) {
+                System.out.println("Maximum tries reached");
+                for(int i=0;i<earlyResultCounter;i++){
+                    CheckResult(UserInput[i], questionz[i].getAnswer());
+                }
+                earlyResultCounter=0;
+                System.out.println("You have Answered "+Score+" questions and Your Score is "+Score*10);
+                return 0;
+            }
             System.out.println("Invalid input, Try again");
             Scanner sc = new Scanner(System.in);
-            Continue(sc.nextInt(),_z);
-        }
-        else{
-        UserInput[_z]=a;
+            return Continue(sc.nextInt(), _z, k);
+        } else {
+            UserInput[_z] = a;
+            earlyResultCounter++;
+            return 1;
         }
     }
-    public void displayQuestion(){
-       for(Question q:questionz){
-        int z=0;
-        System.out.println(q.getId()+":"+q.getQuestions());
-        for(int i=0;i<4;i++){
-            System.out.println(" "+(i+1)+" "+q.getOption()[i]);
+    public void CheckResult(int _UserInput, int _Answer){
+        if(_UserInput==_Answer){
+            Score++;
         }
-        Scanner sc = new Scanner(System.in);
-        Continue(sc.nextInt(),z);
-         z++;
-        System.out.println();
-       }
+    }
+
+    public int displayQuestion() {
+        for (Question q : questionz) {
+            int k = 0;
+            System.out.println(q.getId() + ":" + q.getQuestions());
+            for (int i = 0; i < 4; i++) {
+                System.out.println(" " + (i + 1) + " " + q.getOption()[i]);
+            }
+            Scanner sc = new Scanner(System.in);
+            int counter = Continue(sc.nextInt(), z, k);
+            System.out.println(counter);
+            if (counter == 0) {
+                return 0;
+            }
+            z++;
+            System.out.println();
+        }
+        for(int i=0;i<UserInput.length;i++){
+            CheckResult(UserInput[i], questionz[i].getAnswer());
+        }
+         System.out.println("You have Answered "+Score+" questions and Your Score is "+Score*10);
+        return 0;
     }
 }
